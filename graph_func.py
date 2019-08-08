e877f02a27934f3e40cf69c60da6edffcef313e1
Josie O'Harrow <josieoharrow@hotmail.com>
2019-08-08 09:36:29 -0700

    modulize code

--- graph_func.py
+++ graph_func.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpy.polynomial.polynomial as polynomial
 import scipy
+import make_polynomial as mp
+import sys
 
 ############ Test file for plotting original polynomial function. ############
 
@@ -12,24 +14,13 @@ x_array = np.linspace(x0, x1, 200)
 
 #From this, delta x = 2.
 delta_x = float(x1 - x0)
-delta_x_squared = np.square(delta_x)
-delta_x_cubed = np.power(delta_x, 3)
 
-#Set up the y-based polynomials
-a0, a1, a2, a3 = 1, 0, float(-3)/(delta_x_squared), float(2)/delta_x_cubed
-first_polynomial_consts = [a3, a2, a1, a0]
-second_polynomial_consts = [-a3, a2, a1, a0]
-first_polynomial = scipy.poly1d(np.multiply(y0, first_polynomial_consts))
-second_polynomial = scipy.poly1d(np.multiply(y1, second_polynomial_consts))
-
-#Set up the m-based polynomials
-b0, b1, b2, b3 = 0, 1, float(-2)/delta_x, float(1)/delta_x_squared
-third_polynomial_consts = [b3, b2, b1, b0]
-third_polynomial = scipy.poly1d(np.multiply(m0, third_polynomial_consts))
-#The consts. are different enough I just use new constants
-c0, c1, c2, c3 = 0, float(-1) - float(m1), float(-1)/delta_x, float(m1)/delta_x_squared
-fourth_polynomial_consts = [c3, c2, c1, c0]
-fourth_polynomial = scipy.poly1d(np.multiply(m1, fourth_polynomial_consts))
+############ Set up component polynomial functions ############
+consts = mp.Constants(delta_x)
+first_polynomial = mp.scale_polynomial(y0, mp.make_cubic(consts.y0_consts()))
+second_polynomial = mp.scale_polynomial(y1, mp.make_cubic(consts.y1_consts()))
+third_polynomial = mp.scale_polynomial(m0, mp.make_cubic(consts.m0_consts()))
+fourth_polynomial = mp.scale_polynomial(m1, mp.make_cubic(consts.m1_consts()))
 
 #Declare the base function
 f = first_polynomial(x_array - x0) + second_polynomial(x_array - x1) + third_polynomial(x_array - x0) + fourth_polynomial(x_array - x1)
@@ -46,11 +37,9 @@ f_magnitude = (c(1) - c(0))
 
 #just the values, we want the function.
 p = f/f_magnitude
-#p_func is broken because it is derived from f_func. But it wiiiill work :,)
 p_func = f_func/f_magnitude
 
 #Print info
-print('a0, a1, a2, a3: ', a0, a1, a2, a3)
 print('========== Polynomials: ==========')
 print(first_polynomial)
 print(second_polynomial)
@@ -62,15 +51,16 @@ print('========== F magnitude: ==========')
 print(f_magnitude)
 
 #Plot
-plt.plot(x_array, first_polynomial(x_array - x0))
-plt.plot(x_array, second_polynomial(x_array - x1))
-plt.plot(x_array, third_polynomial(x_array - x0))
-plt.plot(x_array, fourth_polynomial(x_array - x1))
-#Commented out for now because they are all messed up
-#plt.plot(x_array, f_func(x_array - x1))
-#plt.plot(x_array, p_func(x_array - x1))
-plt.plot(x_array, p)
-plt.plot(x_array, f)
+if(len(sys.argv) > 1):
+    plt.plot(x_array, first_polynomial(x_array - x0))
+    plt.plot(x_array, second_polynomial(x_array - x1))
+    plt.plot(x_array, third_polynomial(x_array - x0))
+    plt.plot(x_array, fourth_polynomial(x_array - x1))
+    #Commented out for now because they are all messed up
+    #plt.plot(x_array, f_func(x_array - x1))
+    #plt.plot(x_array, p_func(x_array - x1))
+    plt.plot(x_array, p)
+    plt.plot(x_array, f)
 
-plt.ylabel('Derived polynomial functions')
-plt.show()    
+    plt.ylabel('Derived polynomial functions')
+    plt.show()    
---
+++ make_polynomial.py
@@ -0,0 +1,39 @@
+import scipy
+import numpy as np
+
+#constants array like: [a0, a1, a2, a3]
+def make_cubic(constants_array):
+    a3 = constants_array[3]
+    a2 = constants_array[2]
+    a1 = constants_array[1]
+    a0 = constants_array[0]
+    cubic_consts = [a3, a2, a1, a0]
+    cubic = scipy.poly1d(cubic_consts)
+    return cubic
+
+def scale_polynomial(scalar, polynomial):
+    return np.polymul(scalar, polynomial)
+
+class Constants:
+
+    delta_x = None
+    delta_x_squared = None#np.square(delta_x)
+    delta_x_cubed = None#np.power(delta_x, 3)
+
+    def __init__(self, delta_x): 
+            self.delta_x = delta_x
+            self.delta_x_squared = np.square(delta_x)
+            self.delta_x_cubed = np.power(delta_x, 3)
+    
+    def y0_consts(self):
+        return [1, 0, float(-3)/(self.delta_x_squared), float(2)/self.delta_x_cubed]
+
+    def y1_consts(self):
+        return [1, 0, float(-3)/(self.delta_x_squared), -float(2)/self.delta_x_cubed]
+    
+    def m0_consts(self):
+        return [0, 1, float(-2)/self.delta_x, float(1)/self.delta_x_squared]
+
+    def m1_consts(self):
+        return [0, float(1), float(2)/self.delta_x, float(1)/self.delta_x_squared]
+    
